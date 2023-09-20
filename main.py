eedb40450ed8b36c72de267bf341fa926ef595fd
PortableM <smirsaliev2@gmail.com>
2023-09-20 18:58:19 +0300

    Test function
    Testing performance

--- main.py
+++ main.py
@@ -4,30 +4,23 @@ import time
 from Parser import Parser
 from multiprocessing import Pool, cpu_count
 
-PARSE_AMOUNT = 1000
+
+PARSE_AMOUNT = 10
 
 
-def init_parsers(file_name):
-    """Возвращает список парсеров для каждого канала"""
-    parsers = []
-
+def get_channels_from_json(file_name: str, limit: int = 0) -> list:
+    """Возвращает список каналов из json"""
     with open(file_name) as file:
         data = json.load(file)
-        for channel_data in data:
-            channel_name = channel_data['alias'][1:]
-            parsers.append(Parser(channel_name, default_page=False))
-            if len(parsers) >= PARSE_AMOUNT:
-                break
-
-    return parsers
+        if limit == 0:
+            return [channel_data['alias'][1:] for channel_data in data]
+        return [channel_data['alias'][1:] for channel_data in data[:limit + 1]]
 
 
-def output_text(text, output_file):
-    """Записывает текст в отдельный файл"""
-    with open(output_file, "a") as file:
-        current_time = time.ctime(time.time())
-        file.write("Время записи: " + current_time)
-        file.write(text)
+def init_parsers(channels: list) -> list:
+    """Возвращает список парсеров для каждого канала"""
+    return [Parser(channel_name, default_page=False)
+            for channel_name in channels]
 
 
 def parse_channel(channel_parser):
@@ -38,38 +31,36 @@ def parse_channel(channel_parser):
     return f"{channel_parser.get_name()}: {count}"
 
 
-def parse_subscribers_count():
-    parsers = init_parsers('channels.json')
-    print('Начинаем парсировать...')
+def output_text(text, output_file):
+    """Записывает текст в отдельный файл"""
+    with open(output_file, "a") as file:
+        current_time = time.ctime(time.time())
+        file.write("Время записи: " + current_time)
+        file.write(text)
 
-    # Запускаем несколько процессов
+
+def performance_test():
+    channels_names = get_channels_from_json('channels.json',
+                                            limit=PARSE_AMOUNT)
+    parsers = init_parsers(channels_names)
+
+    # Включаем мультипроцессинг
     num_cores = cpu_count()
     print(f"Использовано ядер: {num_cores}")
     pool = Pool(processes=num_cores)
 
-    # Запускаем parse_channel параллельно
-    results = pool.map(parse_channel, parsers)
-    subscribers_count = '\n'.join(results)
+    # Количество субскриберов для каждого канала
+    subscribers_count_list = pool.map(parse_channel, parsers)
 
-    # Принт резузльтатов в файл
-    output_text(subscribers_count, 'subscribers.txt')
-
-
-def performance_test():
-    """Замер скорости работы"""
-    print("Запуск бота...")
-    start_time = time.time()
-    parse_subscribers_count()
-    end_time = time.time()
-    print("Данные собраны!")
-    print(f"Спарсили {PARSE_AMOUNT} каналов за {end_time - start_time} секунды")
+    # Принт результатов
+    output_text('\n'.join(subscribers_count_list), 'subscribers.txt')
 
 
 if __name__ == '__main__':
     performance_test()
 
     # Устанавливаем задачи в schedular.
-    schedule.every().hour.do(parse_subscribers_count)
+    schedule.every().hour.do(performance_test)
 
     # Поиск задач для выполнения в schedular каждую секунду.
     while True:
